df6e3af856cc956a8e1572515a3335fae1131efa
Eric Bayasgalan <ewiz1020@gmail.com>
2019-12-22 09:21:36 -0800

    minor change

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -5,7 +5,7 @@ const Meta = () => (
     <meta name='viewport' content='width=device-width, initial-scale=1' />
     <meta charSet='utf-8' />
     <link rel='stylesheet' type='text/css' href='/nprogress.css' />
-    <title>Pizzeria</title>
+    <title>Golden Gate Pizzeria</title>
   </Head>
 );
 
