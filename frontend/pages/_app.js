fd0134560c34c27bc3d98d18a5e2ad4bcf82fac6
Eric Bayasgalan <ewiz1020@gmail.com>
2019-12-27 21:42:42 -0800

    opting in for automatic static optimization

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,14 +4,14 @@ import { ApolloProvider } from 'react-apollo';
 import withData from '../lib/withData';
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-    pageProps.query = ctx.query;
-    return { pageProps };
-  }
+  // static async getInitialProps({ Component, ctx }) {
+  //   let pageProps = {};
+  //   if (Component.getInitialProps) {
+  //     pageProps = await Component.getInitialProps(ctx);
+  //   }
+  //   pageProps.query = ctx.query;
+  //   return { pageProps };
+  // }
 
   render() {
     const { Component, apollo, pageProps } = this.props;
